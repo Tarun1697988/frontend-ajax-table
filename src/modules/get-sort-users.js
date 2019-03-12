419f8a73d32348ee481cb731e78ba8c1e05fb27a
tvorohov <tvorogov100500@gmail.com>
2019-03-12 18:18:50 +0200

    remove consolelog

--- src/modules/get-sort-users.js
+++ src/modules/get-sort-users.js
@@ -1,9 +1,7 @@
 export default async function getSortUsers (userData, callback) {
 	const data = await userData;
 
-	console.log(userData);
 	const sortData = data.sort(callback);
 
-	console.log(sortData);
 	return sortData;
 }
